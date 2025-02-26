14c4711e8e40b03cd33ffbc721f69a70e44bbfba
Lee <zenleegaming@gmail.com>
2025-02-25 20:52:02 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
+import PackOpening from "./PackOpening";
 
 function App() {
-    return (
-        <div>
-            <h1>PEV Racers Pack Opening</h1>
-            <p>Welcome to the pack opening experience!</p>
-        </div>
-    );
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>PEV Racers Pack Opening</h1>
+      <PackOpening />
+    </div>
+  );
 }
 
 export default App;
