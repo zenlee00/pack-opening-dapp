2832b3ea5fdcefb2fde7153ddf6ae0b884af205f
Lee <zenleegaming@gmail.com>
2025-02-25 20:32:57 -0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+    return (
+        <div>
+            <h1>PEV Racers Pack Opening</h1>
+            <p>Welcome to the pack opening experience!</p>
+        </div>
+    );
+}
+
+export default App;
